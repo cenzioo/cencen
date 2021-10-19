41b7cc25b7a35bc2be494595cc3a7c94d9cae398
Cenzio <32873789+cenzioo@users.noreply.github.com>
2021-10-19 16:57:18 +0700

    Update index.esm.js

--- CSS/bootstrap/js/index.esm.js
+++ CSS/bootstrap/js/index.esm.js
@@ -16,17 +16,3 @@ import ScrollSpy from './src/scrollspy'
 import Tab from './src/tab'
 import Toast from './src/toast'
 import Tooltip from './src/tooltip'
-
-export {
-  Alert,
-  Button,
-  Carousel,
-  Collapse,
-  Dropdown,
-  Modal,
-  Popover,
-  ScrollSpy,
-  Tab,
-  Toast,
-  Tooltip
-}
